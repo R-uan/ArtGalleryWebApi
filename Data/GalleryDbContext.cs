8027dd4ccb20719f15aedadd083c80f02c26e937
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-08 13:02:22 -0300

    database

---
+++ Data/GalleryDbContext.cs
@@ -0,0 +1,29 @@
+﻿using ArtGallery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtGallery {
+	public class GalleryDbContext : DbContext {
+		public GalleryDbContext(DbContextOptions<GalleryDbContext> options) : base(options) { }
+		public DbSet<Artist> Artists { get; set; }
+		public DbSet<Museum> Museums { get; set; }
+		public DbSet<Artwork> Artworks { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			modelBuilder.Entity<Artist>().ToTable("Artists");
+			modelBuilder.Entity<Museum>().ToTable("Museums");
+			modelBuilder.Entity<Artwork>().ToTable("Artworks");
+
+			modelBuilder.Entity<Artist>()
+			.HasMany(artworks => artworks.Artworks)
+			.WithOne(artist => artist.Artist)
+			.HasForeignKey(key => key.ArtistId)
+			.OnDelete(DeleteBehavior.SetNull);
+
+			modelBuilder.Entity<Museum>()
+			.HasMany(artwork => artwork.Artworks)
+			.WithOne(museum => museum.Museum)
+			.HasForeignKey(key => key.MuseumId)
+			.OnDelete(DeleteBehavior.SetNull);
+		}
+	}
+}
---
+++ Migrations/20240507233611_initial_migration_07_05_2024.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ArtGallery.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration_07_05_2024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Slug = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Movement = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Date_of_death = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Museums",
+                columns: table => new
+                {
+                    MuseumId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Inauguration_year = table.Column<int>(type: "integer", nullable: false),
+                    Slug = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    State = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Latitude = table.Column<int>(type: "integer", nullable: false),
+                    Longitude = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Museums", x => x.MuseumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Artworks",
+                columns: table => new
+                {
+                    ArtworkId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Slug = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ArtistId = table.Column<int>(type: "integer", nullable: false),
+                    MuseumId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artworks", x => x.ArtworkId);
+                    table.ForeignKey(
+                        name: "FK_Artworks_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Artworks_Museums_MuseumId",
+                        column: x => x.MuseumId,
+                        principalTable: "Museums",
+                        principalColumn: "MuseumId",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_ArtistId",
+                table: "Artworks",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artworks_MuseumId",
+                table: "Artworks",
+                column: "MuseumId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artworks");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Museums");
+        }
+    }
+}
