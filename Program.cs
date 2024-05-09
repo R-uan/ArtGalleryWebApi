499b75da4baca33964674b1b4b7d6e62868110e0
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-09 17:44:46 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,44 +1,37 @@
 using ArtGallery;
+using FluentValidation;
 using ArtGallery.Models;
+using ArtGallery.Services;
 using ArtGallery.Repositories;
 using ArtGallery.Utils.Validators;
-using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using ArtGallery.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 var Configuration = builder.Configuration;
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-/* Artists */
-builder.Services.AddScoped<IValidator<Artist>, ArtistValidator>();
-builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
-builder.Services.AddScoped<IArtistService, ArtistService>();
-
+builder.Services.Configure<RouteOptions>(options => {
+	options.ConstraintMap.Add("string", typeof(string));
+});
 
 builder.Services.AddDbContext<GalleryDbContext>(options => {
 	options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+builder.Services.AddScoped<IValidator<Artist>, ArtistValidator>();
+builder.Services.AddScoped<IArtistService, ArtistService>();
+
 var app = builder.Build();
 app.MapControllers();
+
 if (app.Environment.IsDevelopment()) {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+
 }
-
-app.MapGet("/", async context => {
-	try {
-		using (var scope = app.Services.CreateScope()) {
-			var dbContext = scope.ServiceProvider.GetRequiredService<GalleryDbContext>();
-			await dbContext.Database.OpenConnectionAsync();
-			await context.Response.WriteAsync($"Connection to PostgreSQL database successful!");
-		}
-	} catch (Exception ex) {
-		await context.Response.WriteAsync($"Error: {ex.Message}");
-	}
-});
-
-
 
 app.Run();
