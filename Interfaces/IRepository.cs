379f6cd7462f1803633afd58b1ed56a719b38de7
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-09 23:43:29 -0300

    generic interfaces

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtGallery {
+	public interface IRepository<TEntity, UEntity, PEntity> {
+		Task<TEntity> SaveOne(TEntity TEntity);
+		Task<bool?> DeleteById(int id);
+		Task<bool?> UpdateById(int id, UEntity patch);
+
+		Task<TEntity?> FindById(int id);
+		Task<TEntity?> FindBySlug(string slug);
+
+		Task<List<TEntity>> FindAll();
+		Task<List<PEntity>> FindAllPartial();
+	}
+}
---
+++ Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿namespace ArtGallery {
+	public interface IService<TEntity, UEntity, PEntity> {
+		public Task<TEntity> PostOne(TEntity artist);
+		public Task<TEntity?> GetOneById(int id);
+		public Task<TEntity?> GetOneBySlug(string slug);
+
+		public Task<List<TEntity>> GetAll();
+		public Task<List<PEntity>> GetAllPartial();
+
+		public Task<bool?> DeleteOne(int id);
+		public Task<bool?> UpdateOne(int id, UEntity artist);
+	}
+}
