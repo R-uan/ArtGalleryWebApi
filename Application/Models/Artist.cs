e393fbaa60ee8f6234ad7c3acff36ca4c941e685
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-11 22:13:11 -0300

    other

--- Application/Models/Artist.cs
+++ Application/Models/Artist.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ArtGallery.Models {
 	public class Artist {
