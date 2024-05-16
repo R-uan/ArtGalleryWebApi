1febdca6f3264e3d0677942f90eb8646e3cb0db4
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-16 11:46:05 -0300

    more model stuff

--- ArtGallery/Application/Models/UpdateModels/UpdateArtist.cs
+++ ArtGallery/Application/Models/UpdateModels/UpdateArtist.cs
@@ -4,7 +4,7 @@
 		public string? Slug { get; set; }
 		public string? Country { get; set; }
 		public string? Movement { get; set; }
-		public string? Description { get; set; }
+		public string? Biography { get; set; }
 		public string? Profession { get; set; }
 		public DateTime? Date_of_birth { get; set; }
 		public DateTime? Date_of_death { get; set; }
--- ArtGallery/Application/Models/UpdateModels/UpdateArtwork.cs
+++ ArtGallery/Application/Models/UpdateModels/UpdateArtwork.cs
@@ -1,10 +1,10 @@
 ﻿namespace ArtGallery.Models {
 	public class UpdateArtwork {
 		public int? Year { get; set; }
-
+		public string? Period {  get; set; }
 		public string? Slug { get; set; }
 		public string? Title { get; set; }
-		public string? Description { get; set; }
+		public string? History { get; set; }
 		public string? ImageURL { get; set; }
 
 		public int? ArtistId { get; set; }
