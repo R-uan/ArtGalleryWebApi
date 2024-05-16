c8dfb06a30579fb69cf1d0e8b3cf45d04dea7b6f
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-16 11:44:47 -0300

    migrations

---
+++ ArtGallery/Migrations/20240516132045_16_06_2024.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtGallery.Migrations
+{
+    /// <inheritdoc />
+    public partial class _16_06_2024 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MuseumId",
+                table: "Museums",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Artworks",
+                newName: "Period");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtworkId",
+                table: "Artworks",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Artists",
+                newName: "Biography");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtistId",
+                table: "Artists",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "History",
+                table: "Artworks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "History",
+                table: "Artworks");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Museums",
+                newName: "MuseumId");
+
+            migrationBuilder.RenameColumn(
+                name: "Period",
+                table: "Artworks",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Artworks",
+                newName: "ArtworkId");
+
+            migrationBuilder.RenameColumn(
+                name: "Biography",
+                table: "Artists",
+                newName: "Description");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Artists",
+                newName: "ArtistId");
+        }
+    }
+}
---
+++ ArtGallery/Migrations/20240516133016_16_06_2024_2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtGallery.Migrations
+{
+    /// <inheritdoc />
+    public partial class _16_06_2024_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Museums",
+                newName: "MuseumId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Artworks",
+                newName: "ArtworkId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Artists",
+                newName: "ArtistId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MuseumId",
+                table: "Museums",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtworkId",
+                table: "Artworks",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "ArtistId",
+                table: "Artists",
+                newName: "Id");
+        }
+    }
+}
