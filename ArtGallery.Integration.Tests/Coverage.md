86b27e517d9e43d9d45681af43344e1a429f2f6b
Ruan <ruan.oliveiralopes@hotmail.com>
2024-05-19 01:49:04 -0300

    testes de integração

--- ArtGallery.Integration.Tests/Coverage.md
+++ ArtGallery.Integration.Tests/Coverage.md
@@ -1,8 +1,35 @@
 ﻿# Artist Controller
+
+###
+
 -	Get All ✓
 -	Get All Partial ✓
 -	Get One By Id ✓
 -	Get One By Slug ✓
--	Post One
--	Update One
--	Delete One
+-	Post One ✓
+-	Update One ✓
+-	Delete One ✓
+
+###
+
+# Museum Controller
+
+-	Get All ✓
+-	Get All Partial ✓
+-	Get One By Id ✓
+-	Get One By Slug ✓
+-	Post One ✓
+-	Update One ✓
+-	Delete One ✓
+
+###
+
+# Artwork Controller
+
+-	Get All ✓
+-	Get All Partial ✓
+-	Get One By Id ✓
+-	Get One By Slug ✓
+-	Post One ✓
+-	Update One ✓
+-	Delete One ✓
--- ArtGallery.Integration.Tests/Seeders/MuseumSeeder.cs
+++ ArtGallery.Integration.Tests/Seeders/MuseumSeeder.cs
@@ -9,8 +9,8 @@ namespace ArtGallery.Integration.Tests.Seeders {
     public class MuseumSeeder {
         public static async Task Seed(GalleryDbContext context) {
             Museum one = new() { Name = "Belladona", Slug = "museum-belladona", Country = "France" };
-            Museum two = new() { Name = "Belladona", Slug = "museum-belladona", Country = "France" };
-            Museum three = new() { Name = "Belladona", Slug = "museum-belladona", Country = "France" };
+            Museum two = new() { Name = "Louvre", Slug = "museum-louvre", Country = "France" };
+            Museum three = new() { Name = "Caxias", Slug = "museum-caxias", Country = "Brazil" };
             List<Museum> museums = new List<Museum>() { one, two, three };
             await context.Museums.AddRangeAsync(museums);
             await context.SaveChangesAsync();
