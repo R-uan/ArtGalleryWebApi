cea3938105d95272409ff49bb694fa3d2faab669
Ruan <75811206+R-uan@users.noreply.github.com>
2024-06-15 15:32:27 -0300

    Format

--- ArtGallery/Application/Repositories/RedisRepository.cs
+++ ArtGallery/Application/Repositories/RedisRepository.cs
@@ -4,17 +4,20 @@ using StackExchange.Redis;
 
 namespace ArtGallery;
 
-public class RedisRepository(IConnectionMultiplexer muxer) : IRedisRepository {
+public class RedisRepository(IConnectionMultiplexer muxer) : IRedisRepository
+{
 	private readonly IDatabase _redis = muxer.GetDatabase();
 
-	public async Task<bool> Store<T>(string key, T values) {
+	public async Task<bool> Store<T>(string key, T values)
+	{
 		string serialize = JsonSerializer.Serialize(values);
-        TimeSpan expiration = TimeSpan.FromMinutes(30);
-        var store = await _redis.StringSetAsync(key, serialize, expiration);
+		TimeSpan expiration = TimeSpan.FromMinutes(30);
+		var store = await _redis.StringSetAsync(key, serialize, expiration);
 		return store;
 	}
 
-	public async Task<T?> Get<T>(string key) {
+	public async Task<T?> Get<T>(string key)
+	{
 		string? cache = await _redis.StringGetAsync(key);
 		if (cache == null) return default;
 		var data = JsonSerializer.Deserialize<T>(cache);
--- ArtGallery/Interfaces/Services/IArtworkService.cs
+++ ArtGallery/Interfaces/Services/IArtworkService.cs
@@ -3,7 +3,8 @@ using ArtGallery.Models;
 using ArtGallery.Utils;
 namespace ArtGallery.Interfaces;
 
-public interface IArtworkService : IBaseService<Artwork, ArtworkDTO, UpdateArtworkDTO, PartialArtworkDTO> {
-	Task<PaginatedResponse<PartialArtworkDTO>> PaginatedQuery(ArtworkQueryParams queryParams, int page);
+public interface IArtworkService : IBaseService<Artwork, ArtworkDTO, UpdateArtworkDTO, PartialArtworkDTO>
+{
+	Task<PaginatedResponse<PartialArtworkDTO>> PaginatedQuery(ArtworkQueryParams queryParams, int pageIndex);
 
 }
