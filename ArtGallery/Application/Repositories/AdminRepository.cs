a5aa0206cef3fb1550ccd635fc817dfa374e48fd
Ruan <75811206+R-uan@users.noreply.github.com>
2024-06-14 22:38:32 -0300

    Irrelevant

--- ArtGallery/Application/Repositories/AdminRepository.cs
+++ ArtGallery/Application/Repositories/AdminRepository.cs
@@ -2,17 +2,21 @@ using ArtGallery.Interfaces;
 using ArtGallery.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace ArtGallery.Repositories {
-	public class AdminRepository(GalleryDbContext context) : IAdminRepository {
+namespace ArtGallery.Repositories
+{
+	public class AdminRepository(GalleryDbContext context) : IAdminRepository
+	{
 		private readonly GalleryDbContext _db = context;
 
-		public async Task<int?> AddOneAdmin(Admin admin) {
+		public async Task<int?> AddOneAdmin(Admin admin)
+		{
 			var add = await _db.Admin.AddAsync(admin);
 			await _db.SaveChangesAsync();
 			return add.Entity.AdminId;
 		}
 
-		public async Task<Admin?> FindByUsername(string username) {
+		public async Task<Admin?> FindByUsername(string username)
+		{
 			return await _db.Admin.Where(admin => admin.Username == username).FirstOrDefaultAsync();
 		}
 	}
