dc38a875701d1485cba465a6bf5e4870b71b1a50
Ruan <75811206+R-uan@users.noreply.github.com>
2024-06-19 16:38:24 -0300

    Safe commit
    I'll try to add Tailwind to the project. This commit is to have a good rollback point in case I mess it up.

---
+++ ArtGallery/Components/Pages/SinglePeriod.razor.cs
@@ -0,0 +1,11 @@
+﻿using ArtGallery.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace ArtGallery.Components.Pages
+{
+    public partial class SinglePeriod : ComponentBase
+    {
+        [Parameter]
+        public required PartialPeriod Period { get; set; }
+    }
+}
